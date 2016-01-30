d2a1c4b744c699dfc7d7a63593e6c6e3d5d3819c
SuitGuy <pjdines1994@gmail.com>
2016-01-30 15:00:03 +0000

    http get message written

--- app/src/main/java/com/software/team5/health_application/MainActivity.java
+++ app/src/main/java/com/software/team5/health_application/MainActivity.java
@@ -3,6 +3,7 @@
 
 package com.software.team5.health_application;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +16,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -43,6 +59,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        AsyncCaller caller = new AsyncCaller();
+        caller.execute();
+
+
     }
 
     @Override
@@ -100,5 +120,66 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private class AsyncCaller extends AsyncTask<Void, Void, Void>
+    {
+        TextView view = (TextView) findViewById(R.id.hellow);
+        String baseUrl = "http://idcr.rippleosi.org/api/patients/9999999000/labresults";
+        HttpResponse response = null;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+
+        }
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            //this method will be running on background thread so don't update UI frome here
+            //do your long running http tasks here,you dont want to pass argument and u can access the parent class' variable url over here
+            // Create http client object to send request to server
+            HttpClient client = new DefaultHttpClient();
+            // Create URL string
+            String URL = baseUrl;
+            // Create Request to server and get response
+            HttpGet httpget= new HttpGet();
+
+
+
+            try {
+                httpget.setURI(new URI(URL));
+                response = client.execute(httpget);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            String res = "";
+            try {
+                InputStream istream = response.getEntity().getContent();
+                if(istream != null){
+                    BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(istream));
+                    String line = "";
+                    while((line = bufferedReader.readLine()) != null)
+                        res += line;
+
+                    istream.close();
+                }else {
+                    view.setText("it Done brokeded");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            view.setText(res);
+        }
     }
 }
--- app/src/main/java/com/software/team5/health_application/RestInterface.java
+++ app/src/main/java/com/software/team5/health_application/RestInterface.java
@@ -3,6 +3,7 @@ package com.software.team5.health_application;
 
 
 import android.os.AsyncTask;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
@@ -20,48 +21,41 @@ import java.net.URISyntaxException;
 /**
  * Created by Paul on 30/01/2016.
  */
-public class RestInterface extends AsyncTask<Void, Void, String>{
+public class RestInterface implements Runnable {
     private String baseUrl;
     private String GetURL;
+    private TextView view;
 
-    public RestInterface(String baseUrl){
+    public RestInterface(TextView view, String baseUrl, String response ){
         this.baseUrl = baseUrl;
+        this.view = view;
     }
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-    }
 
-    @Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
-    }
 
-    @Override
-    protected String doInBackground(Void... urls){
+    public void run(){
         HttpResponse response = null;
-        try {
-            // Create http client object to send request to server
-            HttpClient client = new DefaultHttpClient();
-            // Create URL string
-            String URL = baseUrl;
-            // Create Request to server and get response
-            HttpGet httpget= new HttpGet();
-            httpget.setURI(new URI(URL));
-            response = client.execute(httpget);
-            Toast.makeText(null, response.toString(), Toast.LENGTH_SHORT).show();
+    try {
+        // Create http client object to send request to server
+        HttpClient client = new DefaultHttpClient();
+        // Create URL string
+        String URL = baseUrl;
+        // Create Request to server and get response
+        HttpGet httpget= new HttpGet();
+        httpget.setURI(new URI(URL));
+        response = client.execute(httpget);
 
-            return response.toString();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-        }
-        return "fail";
+        view.setText(response.toString());
+        view.notify();
+    } catch (URISyntaxException e) {
+        e.printStackTrace();
     }
+    catch (ClientProtocolException e) {
+        // TODO Auto-generated catch block
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+    }
+
+}
 
 }
