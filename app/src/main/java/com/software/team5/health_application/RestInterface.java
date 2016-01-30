fa059afab51184c3a35845502bb65a943b82dfd4
SuitGuy <pjdines1994@gmail.com>
2016-01-30 13:35:07 +0000

    changed to use http client

--- app/src/main/java/com/software/team5/health_application/RestInterface.java
+++ app/src/main/java/com/software/team5/health_application/RestInterface.java
@@ -1,30 +1,67 @@
 package com.software.team5.health_application;
 
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.http.ContentType;
-import com.jayway.restassured.response.Response;
 
-import static com.jayway.restassured.RestAssured.*;
 
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by Paul on 30/01/2016.
  */
-public class RestInterface {
+public class RestInterface extends AsyncTask<Void, Void, String>{
     private String baseUrl;
     private String GetURL;
 
-    public RestInterface(String url){
-        this.baseUrl = url;
-        RestAssured.baseURI = url;
-        RestAssured.basePath = "NHS/API/";
+    public RestInterface(String baseUrl){
+        this.baseUrl = baseUrl;
     }
-    public static JSONObject SendGetRequest(JSONObject message) throws JSONException {
-        Response response;
-        response = get(baseURI + basePath);
 
-        return new JSONObject(response.asString());
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
     }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected String doInBackground(Void... urls){
+        HttpResponse response = null;
+        try {
+            // Create http client object to send request to server
+            HttpClient client = new DefaultHttpClient();
+            // Create URL string
+            String URL = baseUrl;
+            // Create Request to server and get response
+            HttpGet httpget= new HttpGet();
+            httpget.setURI(new URI(URL));
+            response = client.execute(httpget);
+            Toast.makeText(null, response.toString(), Toast.LENGTH_SHORT).show();
+
+            return response.toString();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+        return "fail";
+    }
+
 }
